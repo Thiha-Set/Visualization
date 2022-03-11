5e758b20ee5916a9506ac0bb1e42f433653cdffc
Thiha Set <thihaset@mail.adelphi.edu>
2022-03-11 13:12:22 -0500

    Extraction of anomalies based on protocol

--- docs/Java/Driver.java
+++ docs/Java/Driver.java
@@ -2,10 +2,102 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.io.File;
-public class Driver{
-    public static ArrayList<String[]> getData(String path){
-        ArrayList<String[]> output = new ArrayList<>();
 
+//wrapper class for anomaly
+class Anomaly{
+    private String sIP;
+    private String dIP;
+    private String sPort;
+    private String dPort;
+    private String protocol;
+    private String packets;
+    private String bytes;
+    private String flags;
+    private String sTime;
+    private String duration;
+    private String eTime;
+    private String sensor;
+
+    public Anomaly(String sIPVal, String dIPVal, String sPortVal, String dPortVal, String protocolVal, String packetsVal, String bytesVal, String flagsVal, String sTimeVal, String durationVal, String eTimeVal, String sensorVal){
+        this.sIP = sIPVal;
+        this.dIP = dIPVal;
+        this.sPort = sPortVal;
+        this.dPort = dPortVal;
+        this.protocol = protocolVal;
+        this.packets = packetsVal;
+        this.bytes = bytesVal;
+        this.flags = flagsVal;
+        this.sTime = sTimeVal;
+        this.duration = durationVal;
+        this.eTime = eTimeVal;
+        this.sensor = sensorVal;
+    }
+
+    //accessor methods
+    public String getsIP(){
+        return new String(this.sIP);
+    }
+
+    public String getdIP(){
+        return new String(this.dIP);
+    }
+
+    public String getsPort(){
+        return new String(this.sPort);
+    }
+
+    public String getdPort(){
+        return new String(this.dPort);
+    }
+
+    public String getProtocol(){
+        return new String(this.protocol);
+    }
+
+    public String getPackets(){
+        return new String(this.packets);
+    }
+
+    public String getBytes(){
+        return new String(this.bytes);
+    }
+
+    public String getFlags(){
+        return new String(this.flags);
+    }
+
+    public String getsTime(){
+        return new String(this.sTime);
+    }
+
+    public String getDuration(){
+        return new String(this.duration);
+    }
+
+    public String geteTime(){
+        return new String(this.eTime);
+    }
+
+    public String getSensor(){
+        return new String(this.sensor);
+    }
+
+    //toString method
+    //override toString from Obj superclass
+    public String toString(){
+        return this.getsIP()+", "+this.getdIP()+", "+this.getsPort()+", "+this.getdPort()+", "+this.getProtocol()+", "+this.getPackets()+", "+this.getBytes()+", "+this.getFlags()+", "+this.getsTime()+", "+this.getDuration()+", "+this.geteTime()+", "+this.getSensor();
+    }
+}
+
+public class Driver{
+    
+
+    public static ArrayList<Anomaly> getData(String path){
+        ArrayList<Anomaly> output = new ArrayList<>();
+
+        ArrayList<String> expectPorts = new ArrayList<>();
+        expectPorts.add("53");
+        expectPorts.add("88");
         boolean first = true;   //flag var to skip the first line (attributes)
 
         String line = "";       //current line
@@ -20,8 +112,40 @@ public class Driver{
             while((line = buffRead.readLine()) != null){
                 //Skip on reading the first line of the file (attributes)
                 if(!first){
-                    //store each data pair split by commas in instances
-                    output.add(line.split(","));
+
+                    //look for anomalies before adding the line to the output
+                    //we only add lines to instances if there are anomalies
+                    //patterns to look for to spot anomalies? => what criteria to decide if anomaly?
+                    //place some placeholders => EX: Unusual dIP for subnets
+
+                    //most traffic will go to TCP port 80, 443, 135, 67
+                    //Write program to exclude all expected traffic going to those ports
+                    
+                    
+                    //check to see if dPort != 53 or 88
+                    //get all attributes
+                    String sIP = line.split(",")[0];
+                    String dIP = line.split(",")[1];
+                    String sPort = line.split(",")[2];
+                    String dPort = line.split(",")[3];
+                    String protocol = line.split(",")[4];
+                    String packets = line.split(",")[5];
+                    String bytes = line.split(",")[6];
+                    String flags = line.split(",")[7];
+                    String sTime = line.split(",")[8];
+                    String duration = line.split(",")[9];
+                    String eTime = line.split(",")[10];
+                    String sensor = line.split(",")[11];
+                    
+                    //if we can't find the dPort in the list of
+                    //expected ports, then add it to the output
+                    if(expectPorts.indexOf(dPort)==-1){
+                        //create new Anomaly obj
+                        Anomaly curr = new Anomaly(sIP,dIP,sPort,dPort,protocol,packets,bytes,flags,sTime,duration,eTime,sensor);
+
+                        output.add(curr);
+                    }
+
                 }
 
                 //toggle flag var
@@ -62,25 +186,11 @@ public class Driver{
         String path = "C:\\Users\\19175\\Desktop\\MOAF\\Adelphi\\HON 490\\Visualization\\docs\\Java\\netflow_sample";
 
         //vars 
-        ArrayList<String[]> instances = new ArrayList<>();    //an arraylist containing all data split by commas
+        ArrayList<Anomaly> instances = new ArrayList<>();    //an arraylist containing all data split by commas
 
         //fill instances with all the data from the csv file
         instances = getData(path);
 
-        //convert to JSON
-        //try to create a new file in data
-        try{
-            //specify the directory to create the json file
-            File jsonFile = new File("C:\\Users\\19175\\Desktop\\MOAF\\Adelphi\\HON 490\\Visualization\\docs\\jsonFiles\\data.json");
-
-            //only create the file if the file does not exist in the specified location
-            if(jsonFile.createNewFile())
-                System.out.println("File created");
-            else
-                System.out.println("File exists");
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        System.out.println(instances.get(0));
     }
 }
